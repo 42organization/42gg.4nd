029bb11eefd041589fe7ac3c574db75e3c7e2666
류민아 <rmina1030@naver.com>
2023-07-06 10:48:16 +0900

    Initial commit

---
+++ src/test/java/com/example/gg4ndBe/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gg4ndBe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
